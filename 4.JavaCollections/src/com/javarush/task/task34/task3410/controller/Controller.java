a45b7bcaf66e1dde9b17ef47de51a9ca56a8af7d
zemae <qwe123>
2024-02-09 18:08:40 +0300

    //

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -1,9 +1,10 @@
 package com.javarush.task.task34.task3410.controller;
 
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.Model;
 import com.javarush.task.task34.task3410.view.View;
 
-public class Controller {
+public class Controller implements EventListener{
 
     private final View view;
     private final Model model;
@@ -16,5 +17,25 @@ public class Controller {
         this.model = new Model();
         this.view = new View(this);
         view.init();
+    }
+
+    @Override
+    public void move(Direction direction) {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void startNextLevel() {
+
+    }
+
+    @Override
+    public void levelCompleted(int level) {
+
     }
 }
