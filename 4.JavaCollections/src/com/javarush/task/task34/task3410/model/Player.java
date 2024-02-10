2177755887e82dbd7aa21226b5b70189b0b64ac9
zemae <qwe123>
2024-02-10 17:15:54 +0300

    //

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Player.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Player.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.model;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Player extends CollisionObject implements Movable{
@@ -15,7 +16,11 @@ public class Player extends CollisionObject implements Movable{
 
     @Override
     public void draw(Graphics graphics) {
-        graphics.setColor(Color.magenta);
-        graphics.fillOval(this.x, this.y, this.width, this.height);
+        String[] ss = getClass().getPackage().toString().split(" ");
+        String s = ss[1].replace(".", "\\");
+        ImageIcon imageIcon = new ImageIcon("C:\\Users\\zemae\\IdeaProjects\\JavaRushTasks\\4.JavaCollections\\src\\" + s + "\\player2.jpg");
+        Image player = imageIcon.getImage();
+        graphics.setColor(Color.black);
+        graphics.drawImage(player, this.x, this.y, this.width, this.height, Color.BLACK, null);
     }
 }
